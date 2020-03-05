396a6ee7992c7da7cfe5f3b0c1f8305e8eab7773
Serhii Zinkevych <zinkevych@gmail.com>
2020-03-05 18:03:16 +0200

    HW2_Task1

---
+++ src/components/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import Statistics from "./Statistics";
+import FeedbackOptions from "./FeedbackOptions";
+import Section from "./Section";
+import Notification from "./Notification";
+
+export default class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+  handleClick = e => {
+    this.setState({
+      [e.target.name]: this.state[e.target.name] + 1
+    });
+  };
+
+  countTotalFeedback = () =>
+    this.state.good + this.state.neutral + this.state.bad;
+
+  countPositiveFeedbackPercentage = () =>
+    this.state.good
+      ? Math.round((this.state.good / this.countTotalFeedback()) * 100)
+      : 0;
+
+  static defaultProps = {};
+
+  static propTypes = {};
+
+  render() {
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.handleClick}
+          />
+        </Section>
+        {this.countTotalFeedback() ? (
+          <Section title="Statistics">
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          </Section>
+        ) : (
+          <Notification message="No feedback given" />
+        )}
+      </>
+    );
+  }
+}
---
+++ src/components/FeedbackOptions.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <>
+    {options.map(option => (
+      <button type="button" name={option} onClick={onLeaveFeedback}>
+        {option}
+      </button>
+    ))}
+  </>
+);
+
+export default FeedbackOptions;
