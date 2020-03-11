84b58b756c7de973cd790abb8c53b89df2cda9b1
Serhii Zinkevych <zinkevych@gmail.com>
2020-03-11 11:45:02 +0200

    Updates

---
+++ precache-manifest.81f171bc277e6c85fb1a3442262f279e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ee0bf744989a9a34335609ec82652b",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "80247ef8a6fdf79c0220",
+    "url": "/goit-react-hw-02-feedback/static/css/main.8b6043be.chunk.css"
+  },
+  {
+    "revision": "b8dbca08754fd0336bad",
+    "url": "/goit-react-hw-02-feedback/static/js/2.e94ee26e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.e94ee26e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80247ef8a6fdf79c0220",
+    "url": "/goit-react-hw-02-feedback/static/js/main.dfd37408.chunk.js"
+  },
+  {
+    "revision": "8f295445564cac0e313f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.23cf4f98.js"
+  }
+]);
